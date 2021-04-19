6cbae01ecf3f8e98eb75d60172f32b795bbfc632
Yiren <liou830129@gmail.com>
2021-04-19 20:29:52 +0800

    Initial commit

---
+++ js/admin.js
@@ -0,0 +1,285 @@
+const apiPath = 'yiren';
+const url = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1';
+const token = 'lcRXvpHZUEatPTWCoCIGzpW2KC82';
+const headersToken = {
+  headers: {
+    'Authorization': token
+  }
+};
+const orderList = document.querySelector('.orderPage-table');
+const discartAllBtn = document.querySelector('.discartAllBtn');
+const orderStatus = document.querySelector('.orderStatus');
+
+discartAllBtn.addEventListener('click', delAllOrder);
+orderList.addEventListener('click', delOneOrder);
+orderList.addEventListener('click', changeOrderStatus);
+
+
+let orderData = [];
+let piePrimarDark = "#5434A7";
+let piePrimaryLighter = "#6e5aa3";
+let piePrimarylight = "#9D7FEA";
+let piePrimary = "#DACBFF";
+let pieChartColor = [piePrimarDark,piePrimaryLighter,piePrimarylight,piePrimary];
+
+
+init();
+
+//#region 初始化
+function init() {
+  getOrderList();
+}
+//#endregion
+
+//#region  取得訂單清單
+function getOrderList(){
+  axios.get(`${url}/admin/${apiPath}/orders`,headersToken)
+    .then(function(response){
+      orderData = response.data.orders;
+      showOrderList();
+      productsPieChart();
+      productTypePieChart();
+    })
+    .catch(function(error){
+      console.error(error);
+    })
+}
+//#endregion
+
+//#region 顯示訂單清單
+function showOrderList(){
+  orderData = orderData.sort(function (a, b) {
+    return a.createdAt < b.createdAt ? 1 : -1;
+   });
+  let orderItem = `
+    <thead>
+      <tr>
+        <th>訂單編號</th>
+        <th>聯絡人</th>
+        <th>聯絡地址</th>
+        <th>電子郵件</th>
+        <th>訂單品項</th>
+        <th>訂單日期</th>
+        <th>訂單狀態</th>
+        <th>操作</th>
+      </tr>
+    </thead>
+  `;
+  if(orderData.length == 0){
+    orderItem += `<tr><td colspan="8" class="h5 text-center">目前沒有訂單呦</td></tr>`;
+    orderList.innerHTML = orderItem;
+    return;
+  }
+  orderData.forEach(function(item){
+    // 訂單時間轉換
+    let orderDate = new Date(Number(item.createdAt*1000));
+    orderDate = orderDate.toLocaleDateString("zh-TW");
+    // 訂單狀態
+    let orderStatus = item.paid ? '已處理' : '未處理';
+    let orderStatusColor = orderStatus == '已處理'? 'orderDone': 'orderUndone';
+    // 訂購產品項目
+    let orderProducts = '';
+    item.products.forEach(function(product){
+      orderProducts += `<p>${product.title}</p>`;
+    });
+
+    orderItem += `
+      <tr>
+        <td>${item.id}</td>
+        <td>
+          <p>${item.user.name}</p>
+          <p>${item.user.tel}</p>
+        </td>
+        <td>${item.user.address}</td>
+        <td>${item.user.email}</td>
+        <td>${orderProducts}</td>
+        <td class="text-center">${orderDate}</td>
+        <td>
+          <a href="#" data-orderID = "${item.id}" data-orderPaid = ${item.paid} class="orderStatus ${orderStatusColor}">${orderStatus}</a>
+        </td>
+        <td>
+          <input type="button" class="delSingleOrder-Btn" value="刪除" data-jsControll="jsDelOneOrder" data-orderId="${item.id}">
+        </td>
+      </tr>
+    `;
+    orderList.innerHTML = orderItem;
+  });
+}
+//#endregion
+
+//#region 刪除單一訂單
+function delOneOrder(e){
+  if(e.target.getAttribute('data-jsControll') == 'jsDelOneOrder'){
+    axios.delete(`${url}/admin/${apiPath}/orders/${e.target.getAttribute('data-orderId')}`, headersToken)
+      .then(function(response){
+        getOrderList();
+        alert(`[ 已刪除 ] 訂單編號: ${e.target.getAttribute('data-orderId')}`);
+      })
+      .catch(function(error){
+        console.error(error)
+      })
+  }
+}
+//#endregion
+
+//#region 刪除全部訂單
+function delAllOrder(){
+  if(orderData.length == 0){
+    alert("目前沒有訂單呦");
+    return;
+  }
+  
+  axios.delete(`${url}/admin/${apiPath}/orders`,headersToken)
+    .then(function(response){
+      getOrderList();
+    })
+    .catch(function(error){
+      console.error(error)
+    })
+}
+//#endregion
+
+//#region 訂單處理狀況
+function changeOrderStatus(e){
+  e.preventDefault();
+  
+  if(e.target.classList.contains('orderStatus')){
+    let orderID = e.target.getAttribute('data-orderID');
+    let originalStatus = e.target.getAttribute('data-orderPaid');
+    let newStatus;
+    if(originalStatus == 'true'){
+      newStatus = false;
+    }else{
+      newStatus = true;
+    }
+    let putData = {
+      data: {
+        "id": orderID,
+        "paid": newStatus
+      }
+    }
+    axios.put(`${url}/admin/${apiPath}/orders`, putData, headersToken)
+      .then(function(response){
+        getOrderList();
+        alert('[ 已修改 ] 訂單修改完成囉~');
+      })
+      .catch(function(error){
+        console.error(error)
+      })
+  }
+}
+//#endregion
+
+//#region 圓餅圖 - 全品項
+function productsPieChart(){
+  let sellProducts = {};
+  orderData.forEach(function(order){
+    // 計算每個品項的總銷售金額
+    order.products.forEach(function(product){
+      if(sellProducts[product.title] == undefined){
+        sellProducts[product.title] = product.price;
+      }else{
+        sellProducts[product.title] += product.price;
+      }
+    })
+  });
+  // 整理每個品項的銷售資訊
+  let sellProductsList = [];
+  Object.keys(sellProducts).forEach(function(item){
+    let productItem = {
+      title: item,
+      totalPrice: sellProducts[item]
+    };
+    sellProductsList.push(productItem);
+  });
+  // 按照總銷售金額排序
+  sellProductsList.sort(function(a,b){
+    return a.totalPrice < b.totalPrice ? 1 : -1;
+  }); 
+  console.log(sellProductsList);
+  // 判斷產品數量是否大於2項
+  let newSellProductsList = '';
+  if(sellProductsList.length < 4){
+    newSellProductsList = sellProductsList;
+  }else{
+    newSellProductsList = sellProductsList.filter(function(item,index){
+      return index < 3;
+    });
+    let otherTotalPrice = 0;
+    sellProductsList.forEach(function(item,index){
+      if(index >= 3){
+        otherTotalPrice += item.totalPrice;
+      }
+    });
+    let otherProduct = {
+      title: "其他品項",
+      totalPrice: otherTotalPrice
+    }
+    newSellProductsList.push(otherProduct);
+  }
+  // 統整需要pie chart 需要的資料陣列
+  let pieChartArray = [];
+  newSellProductsList.forEach(function(item){
+    let chartArray = [item.title, item.totalPrice];
+    pieChartArray.push(chartArray);
+  });
+
+  c3.generate({
+    bindto: '#productChart',
+    data: {
+        type: "pie",
+        columns: pieChartArray,
+        
+    },
+    color:{
+      pattern: pieChartColor
+  }
+  });
+}
+//#endregion
+
+//#region 圓餅圖 - 全品項類別
+function productTypePieChart(){
+  let sellProductType = {};
+  orderData.forEach(function(order){
+    // 計算每個品項的總銷售金額
+    order.products.forEach(function(product){
+      if(sellProductType[product.category] == undefined){
+        sellProductType[product.category] = product.price;
+      }else{
+        sellProductType[product.category] += product.price;
+      }
+    })
+  });
+  let sellProductTypeList = [];
+  Object.keys(sellProductType).forEach(function(item){
+    let productType = {
+      category: item,
+      totalPrice: sellProductType[item]
+    };
+    sellProductTypeList.push(productType);
+  });
+  // 按照總銷售金額排序
+  sellProductTypeList.sort(function(a,b){
+    return a.totalPrice < b.totalPrice ? 1 : -1;
+  }); 
+  // 統整需要pie chart 需要的資料陣列
+  let productTypePieChartArr = [];
+  sellProductTypeList.forEach(function(item){
+    let chartArray = [item.category, item.totalPrice];
+    productTypePieChartArr.push(chartArray);
+  });
+
+  c3.generate({
+    bindto: '#productTypeChart',
+    data: {
+        type: "pie",
+        columns: productTypePieChartArr,
+        
+    },
+    color:{
+      pattern: pieChartColor
+  }
+  });
+}
+//#endregion
---
+++ js/index.js
@@ -0,0 +1,347 @@
+const apiPath = 'yiren';
+const url = 'https://hexschoollivejs.herokuapp.com/api/livejs/v1';
+
+const productList = document.querySelector('.jsProductList');
+const productSelect = document.querySelector('.jsProductSelect');
+const cartList = document.querySelector('.jsCartList');
+const orderInfoBtn = document.querySelector('.jsOrderInfoBtn'); 
+const addminLogin = document.querySelector('.jsAdminLogin');
+
+
+productSelect.addEventListener('change',filterProduct);
+productList.addEventListener('click',addToCart);
+cartList.addEventListener('click', delAllCartList);
+cartList.addEventListener('click', delCartItem);
+cartList.addEventListener('click', updateItemCount);
+orderInfoBtn.addEventListener('click', sendOrder);
+addminLogin.addEventListener('click', adminValiation);
+
+
+let productData = [];
+let cartData = [];
+let totalPrice = '';
+
+init();
+
+function init(){
+  getProductList();
+  getCartList();
+}
+
+//#region 取得產品列表
+function getProductList(){
+  axios.get(`${url}/customer/${apiPath}/products`)
+    .then(function (response) {
+      productData = response.data.products;
+      showProductList();
+    })
+    .catch(function(error){
+      console.error(error);
+    })
+}
+//#endregion
+
+//#region 顯示產品列表
+function showProductList(){
+  let productItem = '';
+  productData.forEach(function(item){
+    productItem += `
+      <li class="productCard col-md-3">
+        <h4 class="productType">新品</h4>
+        <img src="${item.images}" alt="">
+        <input class="addCardBtn" type="submit" value="加入購物車" data-productId="${item.id}">
+        <h3 class="fontSizeM">${item.title}</h3>
+        <del class="fontSizeM">NT$${(item.origin_price).toLocaleString()}</del>
+        <p class="fontSizeXL fw-bold">NT$${(item.price).toLocaleString()}</p>
+      </li>
+    `;
+  });
+  productList.innerHTML = productItem;
+}
+//#endregion
+
+//#region 篩選產品列表
+function filterProduct(e){
+  let category = e.target.value;
+  if(category == '全部'){
+    showProductList();
+  }else{
+    let productItem = '';
+    productData.forEach(function(item){
+      if(item.category == category){
+        console.log(category);
+        productItem += `
+        <li class="productCard col-md-3">
+          <h4 class="productType">新品</h4>
+          <img src="${item.images}" alt="">
+          <input class="addCardBtn" type="submit" value="加入購物車" data-productId="${item.id}">
+          <h3 class="fontSizeM">${item.title}</h3>
+          <del class="fontSizeM">NT$${(item.origin_price).toLocaleString()}</del>
+          <p class="fontSizeXL fw-bold">NT$${(item.price.toLocaleString())}</p>
+        </li>
+        `;
+      }
+    });
+    productList.innerHTML = productItem;
+  }
+}
+//#endregion
+
+//#region 取得購物車品項
+function getCartList(){
+  axios.get(`${url}/customer/${apiPath}/carts`)
+    .then(function(response){
+      cartData = response.data.carts;
+      totalPrice = (response.data.finalTotal).toLocaleString();
+      showCartList();
+    })
+    .catch(function(error){
+      console.error(error);
+    })
+}
+//#endregion
+
+//#region 顯示購物車品項
+function showCartList(){
+  if(cartData.length == 0){
+    let cartItems = `<p class="text-center">您的購物車是空的呦~</p>`;
+    cartList.innerHTML = cartItems;
+    return;
+  }
+  let cartItems = `
+    <tr>
+      <th width="45%">品項</th>
+      <th width="15%">單價</th>
+      <th width="20%">數量</th>
+      <th width="10%">金額</th>
+      <th width="10%"></th>
+    </tr>
+  `;
+  cartData.forEach(function(item){
+    cartItems += `
+    <tr>
+      <td>
+        <div class="cardItem-title">
+          <img src="${item.product.images}" alt="">
+          <p>${item.product.title}</p>
+        </div>
+      </td>
+      <td>NT$${(item.product.price.toLocaleString())}</td>
+      <td>
+        <div class="d-flex align-itmes-center mr-3">
+        ${item.quantity == 1? `<button type="button" data-cartId="${item.id}" data-btn="delItemCountBtn" class="ItemCountBtn" disabled>–</button>`:`<button type="button" data-cartId="${item.id}" data-btn="delItemCountBtn" class="ItemCountBtn">–</button>`}
+        <p class="mx-2 p-2" id="${item.id}">${item.quantity}</p>
+        <button type="submit" data-cartId="${item.id}" data-btn="addItemCountBtn" class="ItemCountBtn">+</button>
+      </div>
+      </td>
+      <td>NT$${(item.quantity*item.product.price).toLocaleString()}</td>
+      <td class="text-end">
+        <button class="delCartItemBtn" data-productId="${item.id}" data-jsControll="jsDelCartItemBtn">X</button>
+      </td>
+    </tr>
+    `;
+
+  });
+  cartItems += `
+    <tr>
+      <td>
+        <button class="delAllCartItemBtn" data-jsControll="jsDelAllCartItemBtn">刪除所有品項</button>
+      </td>
+      <td></td>
+      <td></td>
+      <td>
+        <p>總金額</p>
+      </td>
+      <td>NT$${totalPrice}</td>
+    </tr>
+  `;
+  cartList.innerHTML = cartItems; 
+}
+//#endregion
+
+//#region 加入購物車
+function addToCart(e){
+  if(e.target.getAttribute('class') == 'addCardBtn'){
+    let addItemQuantity = 1;
+    cartData.forEach(function(item){
+      if(e.target.getAttribute('data-productId') == item.product.id){
+        addItemQuantity += item.quantity;
+        alert('[ 商品數量修改成功 ] 購物車商品數量已修改~');
+      }
+    });
+    
+    let addItem = {
+      data: {
+        "productId": e.target.getAttribute('data-productId'),
+        "quantity": addItemQuantity
+      }
+    };
+    axios.post(`${url}/customer/${apiPath}/carts` , addItem)
+      .then(function(response){
+        getCartList();
+      })
+  }
+}
+//#endregion
+
+//#region 修改購物車商品數量
+function updateItemCount(e){
+  let itemId = e.target.getAttribute('data-cartId');
+  let itemQuantity = Number(document.getElementById(`${itemId}`).textContent);
+
+  // 增加產品數量
+  if(e.target.getAttribute('data-btn') == 'addItemCountBtn'){
+    itemQuantity += 1;
+  }
+  // 減少產品數量
+  if(e.target.getAttribute('data-btn') == 'delItemCountBtn'){
+    if(itemQuantity > 1){
+      itemQuantity -= 1;
+    }   
+  }
+  // patch 資料
+  let updateItem = {
+    data: {
+      "id": itemId,
+      "quantity": itemQuantity
+    }
+  };
+  axios.patch(`${url}/customer/${apiPath}/carts` , updateItem)
+  .then(function(response){
+    getCartList();    
+  }) 
+}
+//#endregion
+
+
+//#region 刪除購物車單品項
+function delCartItem(e){
+  if(e.target.getAttribute('data-jsControll') == 'jsDelCartItemBtn'){
+    axios.delete(`${url}/customer/${apiPath}/carts/${e.target.getAttribute('data-productId')}`)
+      .then(function(response){
+        getCartList();
+      })
+      .catch(function(error){
+        console.error(error)
+      })
+  }
+}
+
+//#endregion
+
+//#region 刪除購物車所有品項
+function delAllCartList(e){
+  if(e.target.getAttribute('data-jsControll') == 'jsDelAllCartItemBtn'){
+    if(cartData.length == 0){
+      alert('您的購物車是空的呦');
+      return;
+    }
+    axios.delete(`${url}/customer/${apiPath}/carts`)
+      .then(function(response){
+        getCartList();
+        alert('[ 已清空購物車 ] 購物車商品已全部移除~ ');
+      })
+      .catch(function(error){
+        console.error(error)
+      })
+  }
+}
+//#endregion
+
+//#region 送出訂單
+function sendOrder(){
+  if(cartData.length == 0){
+    alert('購物車還是空的呦!');
+    return;
+  };
+  formValiation();
+  let customerName = document.querySelector('#customerName').value;
+  let customerPhone = document.querySelector('#customerPhone').value;
+  let customerEmail = document.querySelector('#customerEmail').value;
+  let customerAddress = document.querySelector('#customerAddress').value;
+  let customerPayment = document.querySelector('#customerPayment').value;
+  
+  let customerOrder = {
+    "data": {
+      "user": {
+        "name": customerName,
+        "tel": customerPhone,
+        "email": customerEmail,
+        "address": customerAddress,
+        "payment": customerPayment
+      }
+    }
+  };
+  alert('訂單建立成功囉!');
+  axios.post(`${url}/customer/${apiPath}/orders`, customerOrder)
+    .then(function(response){
+      window.location.reload();
+    })
+}
+//#endregion
+
+//#region 表單驗證
+function formValiation(){
+  const customerInputs = document.querySelectorAll("input[name],select[data=payment]");
+  const customerForm = document.querySelector(".orderInfo-form");
+  const constraints = {
+    姓名: {
+      presence: {
+        message: "必填"
+      }
+    },
+    手機號碼: {
+      presence: {
+        message: "必填"
+      },
+      length: {
+        is: 10,
+        message: "輸入錯誤"
+      }
+    },
+    信箱: {
+      presence: {
+        message: "必填"
+      },
+      email: {
+        message: "格式錯誤"
+      }
+    },
+    寄送地址: {
+      presence: {
+        message: "必填"
+      }
+    },
+    付款方式: {
+      presence: {
+        message: "必填"
+      }
+    },
+  };
+  customerInputs.forEach((item) => {
+    item.nextElementSibling.textContent = '';
+      let errors = validate(customerForm, constraints) || '';
+      if (errors) {
+        Object.keys(errors).forEach(function (keys) {
+          document.querySelector(`[data-message="${keys}"]`).textContent = errors[keys];
+        })
+      }
+  });
+}
+//#endregion
+
+//#region 管理員登入驗證
+function adminValiation(){
+  const adminAccountKey = 'adminAccount@gmail.com';
+  const adminPwdKey = 'adminPwd';
+  let adminAccount = document.querySelector('#adminAccount').value;
+  let adminPassword = document.querySelector('#adminPassword').value;
+
+  if(adminAccount == adminAccountKey && adminPassword == adminPwdKey){
+    location.href = '/admin.html';
+  }else{
+    const adminError = document.querySelector('.adminError');
+    adminError.textContent = '請按找指示輸入帳號密碼呦';
+  }
+}
+//#endregion 
